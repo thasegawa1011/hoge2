09e6f3be445d9be10dd2f9d14071e29a95ea71c7
thasegawa1011 <thasegawa.future@gmail.com>
2025-10-19 10:03:24 +0900

    change

--- js/script.js
+++ js/script.js
@@ -21,6 +21,9 @@ const colorBg =() => {
 	if(color.value === '#ffffff'){
   text.textContent = `カラーコード:${color.value}(white)`;
 	}
+  else if(color.value === '#000000'){
+    text.textContent =`カラーコード：${color.value}(black)`;
+  }
 	else{
 text.textContent = `カラーコード:${color.value}`;
 	}
