37ebd9b628f33f79176122c1db72c827733210be
thasegawa1011 <thasegawa.future@gmail.com>
2025-10-19 15:17:51 +0900

    Add files via upload

---
+++ ColorPicker/js/script.js
@@ -0,0 +1,33 @@
+
+//console.log('準備完了！');
+//console.log(document.querySelector('#colorPicker').value);
+//document.querySelector('#colorText').textContent ='カラーコード';
+//document.querySelector('#colorText').textContent = 'カラーコード' +document.querySelector('#colorPicker').value;
+
+//document.querySelector('#colorText').textContent= `カラーコード:${document.querySelector('#colorPicker').value}`;
+
+//const name = 'Mana';
+//console.log(name);
+
+const text = document.querySelector('#colorText');
+const color = document.querySelector('#colorPicker');
+
+//カラーピッカーを操作した時の一連の動作
+const colorBg =() => {
+  //選択した色を背景色に設定
+  document.body.style.backgroundColor = color.value;
+
+  //カラーコードを表示
+	if(color.value === '#ffffff'){
+  text.textContent = `カラーコード:${color.value}(white)`;
+	}
+  else if(color.value === '#000000'){
+    text.textContent =`カラーコード：${color.value}(black)`;
+  }
+	else{
+text.textContent = `カラーコード:${color.value}`;
+	}
+}
+
+//カラーピッカーが変更されたらcolorBgを発動させる
+color.addEventListener('input',colorBg);
