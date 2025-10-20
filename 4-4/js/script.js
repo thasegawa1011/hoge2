f2696f7ca9b00b6015f95edea8b2f9547c653dba
thasegawa1011 <thasegawa.future@gmail.com>
2025-10-20 21:51:29 +0900

    change

--- 4-4/js/script.js
+++ 4-4/js/script.js
@@ -1,1 +1,12 @@
-hoge
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click', () =>{
+    document.body.classList.toggle('dark-theme');
+
+    if(btn.textContent ==='ダークモードにする'){
+        btn.textContent = 'ライトモードにする';
+    }
+    else{
+        btn.textContent = 'ダークモードにする';
+    }
+});
