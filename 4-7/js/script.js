e0e4746f5cbc22dce3d4d3654ded3ffd56808689
thasegawa1011 <thasegawa.future@gmail.com>
2025-10-21 23:25:17 +0900

    change

--- 4-7/js/script.js
+++ 4-7/js/script.js
@@ -1,7 +1,7 @@
 const text = document.querySelector('#text');
 const count = document.querySelector('#count');
 
-text.addEventLidtener('keyup', () => {
+text.addEventListener('keyup', () => {
     count.textContent = text.value.length;
 
     if(text.value.length >100){
