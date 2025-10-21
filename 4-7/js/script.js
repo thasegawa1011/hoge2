6d8ced17d26653d586d2432c17ee11d977105b54
thasegawa1011 <thasegawa.future@gmail.com>
2025-10-21 22:45:01 +0900

    change

--- 4-7/js/script.js
+++ 4-7/js/script.js
@@ -1,1 +1,13 @@
 const text = document.querySelector('#text');
+const count = document.querySelector('#count');
+
+text.addEventLidtener('keyup', () => {
+    count.textContent = text.ariaValueMax.length;
+
+    if(text.ariaValueMax.length >100){
+        count.classList.add('alert');
+    }
+    else{
+        count.classList.remove('alart');
+    }
+});
