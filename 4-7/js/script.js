d5262e669878db4f77766784525ee418a476f290
thasegawa1011 <thasegawa.future@gmail.com>
2025-10-21 23:32:25 +0900

    too

--- 4-7/js/script.js
+++ 4-7/js/script.js
@@ -2,12 +2,12 @@ const text = document.querySelector('#text');
 const count = document.querySelector('#count');
 
 text.addEventListener('keyup', () => {
-    count.textContent = text.value.length;
+        count.textContent = text.value.length;
 
-    if(text.value.length >100){
-        count.classList.add('alert');
-    }
-    else{
-        count.classList.remove('alart');
-    }
-});
+        if (text.value.length > 100) {
+            count.classList.add('alert');
+        }
+        else {
+            count.classList.remove('alart');
+        }
+    });
