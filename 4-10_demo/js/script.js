60cfba4b4e02e9e9a5afc8ba048b3d10ad864442
thasegawa1011 <thasegawa.future@gmail.com>
2025-10-22 23:28:09 +0900

    script.js の更新

--- 4-10_demo/js/script.js
+++ 4-10_demo/js/script.js
@@ -2,5 +2,5 @@ const isAgreed = document.querySelector('#check');
 const btn =document.querySelector('#btn');
 
 isAgreed.addEventListener('change', () =>{
-    btn.disabled = !isAgreed.Checked;
+    btn.disabled = !isAgreed.checked;
 });
