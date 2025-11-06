4b0c1c6dc57ae5fb46f44339c0afc762d7fb6cd5
thasegawa1011 <thasegawa.future@gmail.com>
2025-11-06 22:20:39 +0900

    2-2

--- 6-8/js/script.js
+++ 6-8/js/script.js
@@ -1,5 +1,4 @@
 const heading = document.querySelector('#heading');
-
 const keyframes = {
     opacity: [0, 1],
 //    translate:['0 100px', 0],
@@ -11,3 +10,18 @@ const options = {
 };
 
 heading.animate(keyframes,options);
+
+const heading2 = document.querySelector('#heading2');
+const keyframes2 = {
+//    opacity: [0, 1],
+//    translate:['0 100px', 0],
+//    rotate: ['x 360deg', 0],
+    color:['#f66','#fc2','#0c6','#0bd']
+};
+const options2 = {
+    duration: 8000,
+    direction: 'alternate'
+    iterarions: Infinity,
+};
+
+heading2.animate(keyframes2,options2);
