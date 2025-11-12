7cc3a21aa8d9067437d599949bacb69a608bba41
thasegawa1011 <thasegawa.future@gmail.com>
2025-11-12 20:19:23 +0900

    change

--- 6-9/js/script.js
+++ 6-9/js/script.js
@@ -1,1 +1,13 @@
 const itemes = document.querySelectorAll('.img-item');
+
+for (let i = 0; i < itemes.length; i++) {
+    const keyframes ={
+        opacity: [0, 1]
+    };
+    const options = {
+        duration: 600,
+        delay: i*300,
+        fill: 'forwards',
+    };
+    items[i].animate(keyframes, options);
+}
